0ab5248e82d59554335ea3846cc39d9050d5f5b4
Jason Lewis <jason@decomplecting.org>
2015-07-30 11:14:37 -0400

    Add more routes from exercism.io api

--- src/exercism_api/routes/submission.clj
+++ src/exercism_api/routes/submission.clj
@@ -11,8 +11,41 @@
   (ok (c/generate-string {:message "ok"})))
 
 (defroutes* submission-routes
+  ;; Exercise routes
+  ;; Origin: api/routes/exercises.rb
+  (GET* "/exercises" []
+        :return [Exercise]
+        :summary "Grab the next exercises"
+        :query-params [key :- String]
+        (dummy))
+  (GET* "/exercises/:track-id" [track-id]
+        :return [Exercise]
+        :summary "Fetch exercises for a track"
+        :query-params [key :- String]
+        (dummy))
+  (GET* "exercises/:track-id/:slug" [track-id slug]
+        :return Exercise
+        :summary "Fetch a specific exercise"
+        :query-params [key :-String]
+        (dummy))
+
+  ;; Problem routes
+  ;; Origin api/routes/problems.rb
   (GET* "/problems" []
         :return [Problem]
         :summary "Problem listing"
         :query-params [key :- String]
+        (dummy))
+
+  ;; Submission routes
+  ;; Origin api/routes/submissions.rb
+  (GET* "/submissions/:language/:slug" [language slug]
+        :return [Submission]
+        :summary "Individual problem submissions"
+        :query-params [key :- String]
+        (dummy))
+
+  (GET* "/submissions/:key" [key]
+        :return Submission
+        :summary "Individual submission, by key"
         (dummy)))
