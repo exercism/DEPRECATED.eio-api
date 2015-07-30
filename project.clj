1812e3bcfc9802beb6d9c228474b451910d3d7ff
Jason Lewis <jason@decomplecting.org>
2015-07-30 18:52:04 -0400

    Update deps

--- project.clj
+++ project.clj
@@ -2,24 +2,24 @@
   :description "An API for Exercism.io"
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
-                 [clj-time "0.9.0"] ; required due to bug in lein-ring
-                 [metosin/compojure-api "0.22.0"]
+                 [clj-time "0.10.0"] ; required due to bug in lein-ring
+                 [metosin/compojure-api "0.22.1"]
                  [pandect "0.5.2"]
                  [environ "1.0.0"]
-                 [ring/ring-json "0.3.1"]
+                 [ring/ring-json "0.4.0"]
                  [ring/ring-devel "1.4.0"]
-                 [http-kit "2.1.18"]
+                 [http-kit "2.1.19"]
                  [cheshire "5.5.0"]
                  [korma "0.4.2"]
-                 [org.clojure/java.jdbc "0.3.7"]
+                 [org.clojure/java.jdbc "0.4.1"]
                  [org.postgresql/postgresql "9.4-1201-jdbc41"]]
   :ring {:handler exercism-api.handler/app}
   :main exercism-api.core
   :uberjar-name "server.jar"
   :profiles {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
-                                  [cheshire "5.3.1"]
+                                  [cheshire "5.5.0"]
                                   [ring-mock "0.1.5"]
-                                  [midje "1.6.3"]]
+                                  [midje "1.7.0"]]
                    :plugins [[lein-ring "0.9.6"]
                              [lein-midje "3.1.3"]
                              [lein-environ "1.0.0"]]}})
